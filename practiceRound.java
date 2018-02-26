da09be4079c7faee304bbbcd5c8a73275a0fdce2
apoorvmishra <apoorvmishra93@gmail.com>
2018-02-26 12:28:05 -0600

    Update practiceRound.java

--- practiceRound.java
+++ practiceRound.java
@@ -1,3 +1,10 @@
+/*There exists a straight line along which cities are built.
+
+Each city is given a number starting from 1. So if there are 10 cities, city 1 has a number 1, city 2 has a number 2,... city 10 has a number 10.
+
+Different buses (named GBus) operate within different cities, covering all the cities along the way. The cities covered by a GBus are represented as 'first_city_number last_city_number' So, if a GBus covers cities 1 to 10 inclusive, the cities covered by it are represented as '1 10'
+
+We are given the cities covered by all the GBuses. We need to find out how many GBuses go through a particular city.*/
 package codeJam.practiceround1;
 
 import java.io.BufferedReader;
