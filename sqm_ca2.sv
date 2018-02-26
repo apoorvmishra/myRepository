46bb0df67f43ef8ce240d8b7248944482b9e7a52
apoorvmishra <apoorvmishra93@gmail.com>
2018-02-26 12:30:09 -0600

    Update sqm_ca2.sv

--- sqm_ca2.sv
+++ sqm_ca2.sv
@@ -1,3 +1,4 @@
+/*Integrating two functionalities of ALU in a single module. First functionality is Square and Modulus and second is Cellular Automata*/
 module part1part2( input logic [7:0]A, input logic [3:0]B, input logic op,
 		output logic [7:0]Y);
 
