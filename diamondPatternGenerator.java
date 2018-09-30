b66f6d83175548d2375d7d3c5c98f6c302c09e1d
apoorvmishra <apoorvmishra93@gmail.com>
2018-09-29 21:05:18 -0500

    Create diamondPatternGenerator.java

---
+++ diamondPatternGenerator.java
@@ -0,0 +1,41 @@
+/* This code prints the diamond pattern
+
+    *  
+   * *  
+  * * *  
+ * * * *  
+* * * * *  
+ * * * *  
+  * * *  
+   * *  
+    * 
+*/ 
+
+package edu.uic;
+
+public class patternDiamond {
+
+    public static void main(String[] args){
+
+        int n = 5;
+        System.out.println("This is a code to print the pattern of a diamond");
+         for(int i=1; i<=n; i++) {
+             for (int j = n-i; j >= 1; j--) {
+                 System.out.print(" ");
+             }
+             for (int k = 1; k <= i; k++) {
+                 System.out.print("* ");
+             }
+             System.out.println(" ");
+         }
+         for(int i =n-1; i>=1; i--){
+             for(int j=1; j<=n-i; j++){
+                 System.out.print(" ");
+             }
+             for(int k =1; k<=i; k++){
+                 System.out.print("* ");
+             }
+             System.out.println(" ");
+         }
+    }
+}
